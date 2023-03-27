3f6eb632309ad8cd40dcad8ec02572413dff91bd
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2023-03-27 16:19:25 +0200

    Rename metric classes with CamelCase format

--- tests/test_metrics_get_config.py
+++ tests/test_metrics_get_config.py
@@ -1,6 +1,6 @@
 from decomon.layers.core import ForwardMode
 from decomon.metrics.loss import DecomonLossFusion, DecomonRadiusRobust
-from decomon.metrics.metric import Adversarial_check, Adversarial_score, Upper_score
+from decomon.metrics.metric import AdversarialCheck, AdversarialScore, UpperScore
 
 
 def test_decomon_loss():
@@ -20,7 +20,7 @@ def test_decomon_loss():
 
 def test_metric():
     ibp, forward, mode, convex_domain = True, False, ForwardMode.AFFINE, {}
-    for cls in [Adversarial_check, Adversarial_score, Upper_score]:
+    for cls in [AdversarialCheck, AdversarialScore, UpperScore]:
         layer = cls(ibp=ibp, forward=forward, mode=mode, convex_domain=convex_domain)
         config = layer.get_config()
         assert config["ibp"] == ibp
