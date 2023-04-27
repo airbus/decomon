a265e77d0927b3203f6c788c6b8aac96a98f9b6f
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2023-04-27 14:34:01 +0200

    Test clone with deel-lip

--- tests/test_clone.py
+++ tests/test_clone.py
@@ -1,15 +1,35 @@
 # creating toy network and assess that the decomposition is correct
 
 
+import numpy as np
 import pytest
 import tensorflow.keras.backend as K
-from tensorflow.keras.layers import Activation, Dense
+from tensorflow.keras.layers import Activation, Dense, Flatten, Input, Reshape
 from tensorflow.keras.models import Sequential
 
-from decomon.layers.core import ForwardMode
+from decomon.layers.core import ForwardMode, get_mode
+from decomon.layers.decomon_reshape import DecomonReshape
 from decomon.models import clone
-from decomon.models.convert import ConvertMethod
+from decomon.models.utils import ConvertMethod, get_ibp_affine_from_method
 from decomon.utils import Slope
+
+try:
+    import deel.lip
+except ImportError:
+    deel_lip_available = False
+else:
+    deel_lip_available = True
+    from deel.lip.activations import GroupSort
+    from deel.lip.layers import (
+        FrobeniusDense,
+        ScaledL2NormPooling2D,
+        SpectralConv2D,
+        SpectralDense,
+    )
+    from deel.lip.model import Sequential as DeellipSequential
+
+
+deel_lip_skip_reason = "deel-lip is not available"
 
 
 def test_convert_1D(n, method, mode, floatx, helpers):
@@ -134,6 +154,22 @@ def test_name_forward():
     assert nb_batman_layers == 2
 
 
+@pytest.mark.skipif(not (deel_lip_available), reason=deel_lip_skip_reason)
+def test_name_forward_deellip():
+    layers = []
+    layers.append(Dense(1, input_dim=1))
+    layers.append(SpectralDense(1, name="superman"))  # specify the dimension of the input space
+    layers.append(Activation("relu"))
+    layers.append(SpectralDense(1, activation=GroupSort(n=1), name="batman"))
+    model = Sequential(layers)
+
+    decomon_model_f = clone(model=model, method=ConvertMethod.FORWARD_HYBRID)
+    nb_superman_layers = len([layer for layer in decomon_model_f.layers if layer.name.startswith("superman_")])
+    assert nb_superman_layers == 1
+    nb_batman_layers = len([layer for layer in decomon_model_f.layers if layer.name.startswith("batman_")])
+    assert nb_batman_layers == 2
+
+
 def test_name_backward():
     layers = []
     layers.append(Dense(1, input_dim=1))
@@ -147,3 +183,61 @@ def test_name_backward():
     assert nb_superman_layers == 2
     nb_batman_layers = len([layer for layer in decomon_model_b.layers if layer.name.startswith("batman_")])
     assert nb_batman_layers == 3
+
+
+@pytest.mark.skipif(not (deel_lip_available), reason=deel_lip_skip_reason)
+def test_name_backward_deellip():
+    layers = []
+    layers.append(Dense(1, input_dim=1))
+    layers.append(SpectralDense(1, name="superman"))  # specify the dimension of the input space
+    layers.append(Activation("relu"))
+    layers.append(SpectralDense(1, activation="relu", name="batman"))
+    model = Sequential(layers)
+
+    decomon_model_b = clone(model=model, method=ConvertMethod.CROWN_FORWARD_HYBRID)
+    nb_superman_layers = len([layer for layer in decomon_model_b.layers if layer.name.startswith("superman_")])
+    assert nb_superman_layers == 2
+    nb_batman_layers = len([layer for layer in decomon_model_b.layers if layer.name.startswith("batman_")])
+    assert nb_batman_layers == 3
+
+
+@pytest.mark.skipif(not (deel_lip_available), reason=deel_lip_skip_reason)
+def test_clone_full_deellip_model_forward(helpers):
+    decimal = 5
+    target_shape = (6, 6, 2)
+    input_shape = (np.prod(target_shape),)
+    model = DeellipSequential(
+        [
+            Reshape(target_shape=target_shape, input_shape=input_shape),
+            # Lipschitz layers preserve the API of their superclass ( here Conv2D )
+            # an optional param is available: k_coef_lip which control the lipschitz
+            # constant of the layer
+            SpectralConv2D(
+                filters=16,
+                kernel_size=(3, 3),
+                activation=GroupSort(2),
+                use_bias=True,
+                kernel_initializer="orthogonal",
+            ),
+            # our layers are fully interoperable with existing keras layers
+            Flatten(),
+            SpectralDense(
+                32,
+                activation=GroupSort(2),
+                use_bias=True,
+                kernel_initializer="orthogonal",
+            ),
+            FrobeniusDense(10, activation=None, use_bias=False, kernel_initializer="orthogonal"),
+        ],
+        # similary model has a parameter to set the lipschitz constant
+        # to set automatically the constant of each layer
+        k_coef_lip=1.0,
+        name="hkr_model",
+    )
+
+    method = ConvertMethod.FORWARD_HYBRID
+    ibp, affine = get_ibp_affine_from_method(method)
+    mode = get_mode(ibp=ibp, affine=affine)
+    decomon_model = clone(model, method=method, final_ibp=ibp, final_affine=affine)
+
+    # check bounds: todo
