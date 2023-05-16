30c1ec64697c6a85ff2331db25ed2098f0953f82
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2023-05-17 00:00:39 +0200

    Fix reset_layer() in decomon layers
    - it was not consistent between DecomonDense, DecomonConv2D,
    and DecomonBatchNormalization
    - it was not working if the Decomon layer was having more weights
      which were not related to finetuning (for instance if we introduce
      tf.Variable in the convex domain)
    
    We now track the names of the weights to be set from the original keras layer
    via the property `keras_weights_names` and check their position in the
    weights list to update accordingly the decomon weights list (using also
    the appropriate position)
    
    New tests have been added to check that reset_layer is working as
    intended in various cases.

---
+++ src/decomon/keras_utils.py
@@ -0,0 +1,45 @@
+from tensorflow import Variable
+from tensorflow.keras.layers import Layer
+
+
+def get_weight_index(layer: Layer, weight: Variable) -> int:
+    """Get weight index among layer tracked weights
+
+    Args:
+        layer: layer we are looking
+        weight: weight supposed to be part of tracked weights by the layer
+
+    Returns:
+        the index of the weight in `layer.weights` list
+
+    Raises:
+        IndexError: if `weight` is not part of `layer.weights`
+
+    """
+    indexes = [i for i, w in enumerate(layer.weights) if w is weight]
+    try:
+        return indexes[0]
+    except IndexError:
+        raise IndexError(f"The weight {weight} is not tracked by the layer {layer}.")
+
+
+def get_weight_index_from_name(layer: Layer, weight_name: str) -> int:
+    """Get weight index among layer tracked weights
+
+    Args:
+        layer: layer we are looking
+        weight_name: name of the weight supposed to be part of tracked weights by the layer
+
+    Returns:
+        the index of the weight in `layer.weights` list
+
+    Raises:
+        AttributeError: if `weight_name` is not the name of an attribute of `layer`
+        IndexError: if the corresponding layer attribute is not part of `layer.weights`
+
+    """
+    weight = getattr(layer, weight_name)
+    try:
+        return get_weight_index(layer=layer, weight=weight)
+    except IndexError:
+        raise IndexError(f"The weight {weight_name} is not tracked by the layer {layer}.")
---
+++ tests/test_decomon_reset_layer.py
@@ -0,0 +1,211 @@
+import numpy as np
+import pytest
+import tensorflow as tf
+from numpy.testing import assert_almost_equal
+from tensorflow.keras.layers import BatchNormalization, Conv2D, Dense, Flatten
+
+from decomon.layers.core import ForwardMode
+from decomon.layers.decomon_layers import (
+    DecomonBatchNormalization,
+    DecomonConv2D,
+    DecomonDense,
+    DecomonFlatten,
+)
+
+
+def test_decomondense_reset_layer(helpers, use_bias):
+    dc_decomp = False
+    input_dim = 1
+    units = 3
+    mode = ForwardMode.HYBRID
+    layer = Dense(units=units, use_bias=use_bias)
+    layer(tf.zeros((2, input_dim)))
+    decomon_layer = DecomonDense(units=units, use_bias=use_bias, mode=mode, dc_decomp=dc_decomp)
+    inputs = helpers.get_tensor_decomposition_1d_box(dc_decomp=dc_decomp)
+    decomon_layer(inputs[2:])
+
+    kernel = layer.kernel
+    layer.kernel.assign(2 * np.ones_like(kernel))
+    decomon_layer.kernel.assign(np.zeros_like(kernel))
+    if use_bias:
+        bias = layer.bias
+        layer.bias.assign(np.ones_like(bias))
+        decomon_layer.bias.assign(np.zeros_like(bias))
+
+    decomon_layer.reset_layer(layer)
+    assert decomon_layer.kernel is not layer.kernel
+    assert_almost_equal(decomon_layer.kernel.numpy(), layer.kernel.numpy())
+    if use_bias:
+        assert len(layer.weights) == 2
+        assert decomon_layer.bias is not layer.bias
+        assert_almost_equal(decomon_layer.bias.numpy(), layer.bias.numpy())
+    else:
+        assert len(layer.weights) == 1
+
+
+def test_decomondense_reset_layer_decomon_with_new_weights(helpers):
+    dc_decomp = False
+    input_dim = 1
+    units = 3
+    mode = ForwardMode.HYBRID
+    layer = Dense(units=units)
+    layer(tf.zeros((2, input_dim)))
+    decomon_layer = DecomonDense(units=units, mode=mode, dc_decomp=dc_decomp)
+    inputs = helpers.get_tensor_decomposition_1d_box(dc_decomp=dc_decomp)
+    decomon_layer(inputs[2:])
+    # Add new variables
+    decomon_layer.add_weight(shape=(input_dim, units), initializer="ones", name="alpha", trainable=True)
+    decomon_layer.add_weight(shape=(input_dim, units), initializer="ones", name="beta", trainable=False)
+    assert len(decomon_layer.weights) == 4
+    assert len(decomon_layer.trainable_weights) == 3
+
+    kernel, bias = layer.get_weights()
+    layer.set_weights([2 * np.ones_like(kernel), np.ones_like(bias)])
+    decomon_layer.kernel.assign(np.zeros_like(kernel))
+    decomon_layer.bias.assign(np.zeros_like(bias))
+
+    decomon_layer.reset_layer(layer)
+    assert decomon_layer.kernel is not layer.kernel
+    assert decomon_layer.bias is not layer.bias
+    assert_almost_equal(decomon_layer.kernel.numpy(), layer.kernel.numpy())
+    assert_almost_equal(decomon_layer.bias.numpy(), layer.bias.numpy())
+
+
+def test_decomondense_reset_layer_keras_with_new_weights(helpers):
+    dc_decomp = False
+    input_dim = 1
+    units = 3
+    mode = ForwardMode.HYBRID
+    layer = Dense(units=units)
+    layer(tf.zeros((2, input_dim)))
+    decomon_layer = DecomonDense(units=units, mode=mode, dc_decomp=dc_decomp)
+    inputs = helpers.get_tensor_decomposition_1d_box(dc_decomp=dc_decomp)
+    decomon_layer(inputs[2:])
+    # Add new variables
+    layer.add_weight(shape=(input_dim, units), initializer="ones", name="alpha", trainable=False)
+    assert len(layer.weights) == 3
+
+    kernel = layer.kernel
+    layer.kernel.assign(2 * np.ones_like(kernel))
+    decomon_layer.kernel.assign(np.zeros_like(kernel))
+    bias = layer.bias
+    layer.bias.assign(np.ones_like(bias))
+    decomon_layer.bias.assign(np.zeros_like(bias))
+
+    decomon_layer.reset_layer(layer)
+    assert decomon_layer.kernel is not layer.kernel
+    assert decomon_layer.bias is not layer.bias
+    assert_almost_equal(decomon_layer.kernel.numpy(), layer.kernel.numpy())
+    assert_almost_equal(decomon_layer.bias.numpy(), layer.bias.numpy())
+
+
+def test_decomondense_reset_layer_ko_keraslayer_not_nuilt():
+    dc_decomp = False
+    input_dim = 1
+    units = 3
+    mode = ForwardMode.HYBRID
+    layer = Dense(units=units)
+    decomon_layer = DecomonDense(units=units, mode=mode, dc_decomp=dc_decomp)
+    with pytest.raises(ValueError):
+        decomon_layer.reset_layer(layer)
+
+
+def test_decomondense_reset_layer_ko_decomonlayer_not_nuilt():
+    dc_decomp = False
+    input_dim = 1
+    units = 3
+    mode = ForwardMode.HYBRID
+    layer = Dense(units=units)
+    layer(tf.zeros((2, input_dim)))
+    decomon_layer = DecomonDense(units=units, mode=mode, dc_decomp=dc_decomp)
+    with pytest.raises(ValueError):
+        decomon_layer.reset_layer(layer)
+
+
+def test_decomonconv2d_reset_layer(helpers, use_bias):
+    dc_decomp = False
+    odd = 0
+    data_format = "channels_last"
+    filters = 3
+    kernel_size = (3, 3)
+    mode = ForwardMode.HYBRID
+
+    inputs = helpers.get_tensor_decomposition_images_box(data_format, odd, dc_decomp=False)
+    layer = Conv2D(filters=filters, kernel_size=kernel_size, use_bias=use_bias)
+    layer(inputs[1])
+    decomon_layer = DecomonConv2D(
+        filters=filters, kernel_size=kernel_size, use_bias=use_bias, mode=mode, dc_decomp=dc_decomp
+    )
+    decomon_layer(inputs[2:])
+
+    kernel = layer.kernel
+    layer.kernel.assign(2 * np.ones_like(kernel))
+    decomon_layer.kernel.assign(np.zeros_like(kernel))
+    if use_bias:
+        bias = layer.bias
+        layer.bias.assign(np.ones_like(bias))
+        decomon_layer.bias.assign(np.zeros_like(bias))
+
+    decomon_layer.reset_layer(layer)
+    assert decomon_layer.kernel is not layer.kernel
+    assert_almost_equal(decomon_layer.kernel.numpy(), layer.kernel.numpy())
+    if use_bias:
+        assert decomon_layer.bias is not layer.bias
+        assert_almost_equal(decomon_layer.bias.numpy(), layer.bias.numpy())
+
+
+@pytest.mark.parametrize(
+    "center, scale",
+    [
+        (True, True),
+        (False, False),
+    ],
+)
+def test_decomonbacthnormalization_reset_layer(helpers, center, scale):
+    if not center or not scale:
+        pytest.xfail("DecomonBatchNormalization does not take into account yet center=False or scale=False")
+
+    dc_decomp = False
+    odd = 0
+    mode = ForwardMode.HYBRID
+    inputs = helpers.get_tensor_decomposition_multid_box(odd=odd, dc_decomp=dc_decomp)
+
+    layer = BatchNormalization(center=center, scale=scale)
+    layer(inputs[1])
+    decomon_layer = DecomonBatchNormalization(center=center, scale=scale, mode=mode, dc_decomp=dc_decomp)
+    decomon_layer(inputs[2:])
+
+    moving_mean = layer.moving_mean
+    layer.moving_mean.assign(np.ones_like(moving_mean))
+    decomon_layer.moving_mean.assign(np.zeros_like(moving_mean))
+    moving_variance = layer.moving_variance
+    layer.moving_variance.assign(np.ones_like(moving_variance))
+    decomon_layer.moving_variance.assign(np.zeros_like(moving_variance))
+    if center:
+        beta = layer.beta
+        layer.beta.assign(np.ones_like(beta))
+        decomon_layer.beta.assign(np.zeros_like(beta))
+    if scale:
+        gamma = layer.gamma
+        layer.gamma.assign(np.ones_like(gamma))
+        decomon_layer.gamma.assign(np.zeros_like(gamma))
+
+    decomon_layer.reset_layer(layer)
+
+    keras_weights = layer.get_weights()
+    decomon_weights = layer.get_weights()
+    for i in range(len(decomon_weights)):
+        assert_almost_equal(decomon_weights[i], keras_weights[i])
+
+
+def test_decomonflatten_reset_layer(helpers):
+    dc_decomp = False
+    input_dim = 1
+    mode = ForwardMode.HYBRID
+    layer = Flatten()
+    layer(tf.zeros((2, input_dim)))
+    decomon_layer = DecomonFlatten(mode=mode, dc_decomp=dc_decomp)
+    inputs = helpers.get_tensor_decomposition_1d_box(dc_decomp=dc_decomp)
+    decomon_layer(inputs[2:])
+
+    decomon_layer.reset_layer(layer)
