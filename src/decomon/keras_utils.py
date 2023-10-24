0d4ee82af49adb50ac18b919a6b8050781bed081
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2023-10-24 10:25:40 +0200

    Move reset_layers in keras_utils to be used elsewhere
    Annotate it also as working on all layers (nothing particular to decomon
    layers was involved)
    Add a version that update all weights of the new layer.

--- src/decomon/keras_utils.py
+++ src/decomon/keras_utils.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import keras
 from keras.layers import Layer
 
@@ -43,3 +45,41 @@ def get_weight_index_from_name(layer: Layer, weight_name: str) -> int:
         return get_weight_index(layer=layer, weight=weight)
     except IndexError:
         raise IndexError(f"The weight {weight_name} is not tracked by the layer {layer}.")
+
+
+def reset_layer(new_layer: Layer, original_layer: Layer, weight_names: List[str]) -> None:
+    """Reset some weights of a layer by using the weights of another layer.
+
+    Args:
+        new_layer: the decomon layer whose weights will be updated
+        original_layer: the layer used to update the weights
+        weight_names: the names of the weights to update
+
+    Returns:
+
+    """
+    if not original_layer.built:
+        raise ValueError(f"the layer {original_layer.name} has not been built yet")
+    if not new_layer.built:
+        raise ValueError(f"the layer {new_layer.name} has not been built yet")
+    else:
+        new_params = new_layer.get_weights()
+        original_params = original_layer.get_weights()
+        for weight_name in weight_names:
+            new_params[get_weight_index_from_name(new_layer, weight_name)] = original_params[
+                get_weight_index_from_name(original_layer, weight_name)
+            ]
+        new_layer.set_weights(new_params)
+
+
+def reset_layer_all_weights(new_layer: Layer, original_layer: Layer) -> None:
+    """Reset all the weights of a layer by using the weights of another layer.
+
+    Args:
+        new_layer: the decomon layer whose weights will be updated
+        original_layer: the layer used to update the weights
+
+    Returns:
+
+    """
+    reset_layer(new_layer=new_layer, original_layer=original_layer, weight_names=[w.name for w in new_layer.weights])
