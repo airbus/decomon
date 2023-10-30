fa6ee26886fd69b79e90a7113063307dfc76df87
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2023-10-30 16:23:43 +0100

    Add default compute_output_shape for backward layers
    This avoid calling call() with keras tensors to deduce the output shape.

--- src/decomon/backward_layers/core.py
+++ src/decomon/backward_layers/core.py
@@ -2,8 +2,10 @@ from abc import ABC, abstractmethod
 from typing import Any, Dict, List, Optional, Tuple, Union
 
 import keras
-from keras.layers import Layer, Wrapper
+import numpy as np
+from keras.layers import InputLayer, Layer, Wrapper
 
+from decomon.backward_layers.utils import get_identity_lirpa_shapes
 from decomon.core import (
     BoxDomain,
     ForwardMode,
@@ -89,6 +91,42 @@ class BackwardLayer(ABC, Wrapper):
         """
         # generic case: nothing to do before call
         pass
+
+    def compute_output_shape(self, input_shape: List[Tuple[Optional[int], ...]]) -> List[Tuple[Optional[int], ...]]:
+        """Compute expected output shape according to input shape
+
+        Will be called by symbolic calls on Keras Tensors.
+
+        Args:
+            input_shape
+
+        Returns:
+
+        """
+        if isinstance(self.layer, DecomonLayer):
+            decomon_input_shape = [inp.shape for inp in self.layer.input]
+            decomon_output_shape = [out.shape for out in self.layer.output]
+            keras_output_shape = self.inputs_outputs_spec.get_kerasinputshape_from_inputshapesformode(
+                decomon_output_shape
+            )
+            keras_input_shape = self.inputs_outputs_spec.get_kerasinputshape_from_inputshapesformode(
+                decomon_input_shape
+            )
+        else:  # Keras layer
+            keras_output_shape = self.layer.output.shape
+            if isinstance(self.layer, InputLayer):
+                keras_input_shape = keras_output_shape
+            else:
+                keras_input_shape = self.layer.input.shape
+
+        batch_size = keras_input_shape[0]
+        flattened_keras_output_shape = int(np.prod(keras_output_shape[1:]))  # type: ignore
+        flattened_keras_input_shape = int(np.prod(keras_input_shape[1:]))  # type: ignore
+
+        b_shape = batch_size, flattened_keras_output_shape
+        w_shape = batch_size, flattened_keras_input_shape, flattened_keras_output_shape
+
+        return [w_shape, b_shape, w_shape, b_shape]
 
     def freeze_weights(self) -> None:
         pass
