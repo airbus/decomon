a73836d86ce03ffe4bc35554f5f0a5e70b6cf65e
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2023-12-14 16:11:02 +0100

    Add utility function to share weights between layers
    This is a priori now not possible by design in keras 3 after the layer
    is built.
    See for instance this issue: https://github.com/keras-team/keras/issues/18419#issuecomment-1731867106
    where it is advised to embed the layer whose weights we want to share.
    
    In our usecase (reproduce a given model by splitting the activations
    into separate layers but keeping the weights to get synchronized with
    original model), this is not a solution.
    
    We implement this workaround, even though using a private method for
    that. A feature request has been done on keras 3 repo:
    https://github.com/keras-team/keras/issues/18821

--- tests/test_keras_utils.py
+++ tests/test_keras_utils.py
@@ -2,7 +2,7 @@ import keras
 import keras.ops as K
 import numpy as np
 import pytest
-from keras.layers import Dense
+from keras.layers import Dense, Input
 from numpy.testing import assert_almost_equal
 
 from decomon.keras_utils import (
@@ -10,6 +10,7 @@ from decomon.keras_utils import (
     BACKEND_TENSORFLOW,
     LinalgSolve,
     get_weight_index_from_name,
+    share_layer_all_weights,
 )
 
 
@@ -58,3 +59,34 @@ def test_linalgsolve(floatx, decimal):
     sol_tensor = LinalgSolve()(matrix_tensor, rhs_tensor)
     assert keras.backend.standardize_dtype(sol_tensor.dtype) == dtype
     assert_almost_equal(expected_sol, keras.ops.convert_to_numpy(sol_tensor), decimal=decimal)
+
+
+def test_share_layer_all_weights_nok_original_layer_unbuilt():
+    original_layer = Dense(3)
+    new_layer = original_layer.__class__.from_config(original_layer.get_config())
+    with pytest.raises(ValueError):
+        share_layer_all_weights(original_layer=original_layer, new_layer=new_layer)
+
+
+def test_share_layer_all_weights_nok_new_layer_built():
+    original_layer = Dense(3)
+    inp = Input((1,))
+    original_layer(inp)
+    new_layer = original_layer.__class__.from_config(original_layer.get_config())
+    new_layer(inp)
+    with pytest.raises(ValueError):
+        share_layer_all_weights(original_layer=original_layer, new_layer=new_layer)
+
+
+def test_share_layer_all_weights_ok():
+    original_layer = Dense(3)
+    inp = Input((1,))
+    original_layer(inp)
+    new_layer = original_layer.__class__.from_config(original_layer.get_config())
+    share_layer_all_weights(original_layer=original_layer, new_layer=new_layer)
+
+    # check same weights
+    assert len(original_layer.weights) == len(new_layer.weights)
+    for w in original_layer.weights:
+        new_w = [ww for ww in new_layer.weights if ww.name == w.name][0]
+        assert w is new_w
