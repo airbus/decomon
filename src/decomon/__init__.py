127be44cef032ff26944d82eb1768ed4762e5f51
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2024-02-01 23:17:56 +0100

    Deactivate imports in decomon.__init__()
    Some imports are not working anymore because of the partial refactoring

--- src/decomon/__init__.py
+++ src/decomon/__init__.py
@@ -8,23 +8,23 @@ from importlib.metadata import PackageNotFoundError, version
 
 import keras
 
-from . import layers, models
-from .metrics.loss import get_adv_loss, get_lower_loss, get_model, get_upper_loss
-from .models.models import get_AB as get_grid_params
-from .models.models import get_AB_finetune as get_grid_slope
-from .wrapper import (
-    check_adv_box,
-    get_adv_box,
-    get_adv_noise,
-    get_lower_box,
-    get_lower_noise,
-    get_range_box,
-    get_range_noise,
-    get_upper_box,
-    get_upper_noise,
-    refine_box,
-)
-from .wrapper_with_tuning import get_lower_box_tuning, get_upper_box_tuning
+# from . import layers, models
+# from .metrics.loss import get_adv_loss, get_lower_loss, get_model, get_upper_loss
+# from .models.models import get_AB as get_grid_params
+# from .models.models import get_AB_finetune as get_grid_slope
+# from .wrapper import (
+#     check_adv_box,
+#     get_adv_box,
+#     get_adv_noise,
+#     get_lower_box,
+#     get_lower_noise,
+#     get_range_box,
+#     get_range_noise,
+#     get_upper_box,
+#     get_upper_noise,
+#     refine_box,
+# )
+# from .wrapper_with_tuning import get_lower_box_tuning, get_upper_box_tuning
 
 try:
     __version__ = version("decomon")
