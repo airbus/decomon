3f4382c36fa8be1bc02edf1e0ea01692f810a08b
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2024-02-12 23:12:25 +0100

    Test DecomonActivation on all kind of inputs (diag/nobatch/identity)

--- tests/test_activation.py
+++ tests/test_activation.py
@@ -7,58 +7,48 @@ from decomon.keras_utils import batch_multid_dot
 from decomon.layers.activations.activation import DecomonActivation
 
 
-@pytest.mark.parametrize("input_shape", [(1,), (3,), (5, 2, 3)], ids=["0d", "1d", "multid"])
 def test_decomon_activation(
     activation,
     slope,
     ibp,
     affine,
     propagation,
-    input_shape,
     perturbation_domain,
     batchsize,
+    keras_symbolic_input_fn,
+    decomon_symbolic_input_fn,
+    keras_input_fn,
+    decomon_input_fn,
     helpers,
 ):
+    decimal = 5
     decomon_layer_class = DecomonActivation
 
-    keras_symbolic_input = Input(input_shape)
+    # init + build keras layer
+    keras_symbolic_input = keras_symbolic_input_fn()
     layer = Activation(activation=activation)
     layer(keras_symbolic_input)
-    output_shape = layer.output.shape[1:]
 
-    decomon_symbolic_inputs = helpers.get_decomon_symbolic_inputs(
-        model_input_shape=input_shape,
-        model_output_shape=output_shape,
-        layer_input_shape=input_shape,
-        layer_output_shape=output_shape,
-        ibp=ibp,
-        affine=affine,
-        propagation=propagation,
-        perturbation_domain=perturbation_domain,
-    )
+    # init + build decomon layer
+    output_shape = layer.output.shape[1:]
+    model_output_shape = output_shape
+    decomon_symbolic_inputs = decomon_symbolic_input_fn(output_shape=output_shape)
     decomon_layer = decomon_layer_class(
         layer=layer,
         ibp=ibp,
         affine=affine,
         propagation=propagation,
         perturbation_domain=perturbation_domain,
-        model_output_shape=output_shape,
+        model_output_shape=model_output_shape,
         slope=slope,
     )
     decomon_layer(decomon_symbolic_inputs)
 
-    keras_input = helpers.generate_random_tensor(input_shape, batchsize=batchsize)
-    decomon_inputs = helpers.generate_simple_decomon_layer_inputs_from_keras_input(
-        keras_input=keras_input,
-        layer_output_shape=output_shape,
-        ibp=ibp,
-        affine=affine,
-        propagation=propagation,
-        perturbation_domain=perturbation_domain,
-    )
+    # call on actual inputs
+    keras_input = keras_input_fn()
+    decomon_inputs = decomon_input_fn(keras_input=keras_input, output_shape=output_shape)
 
     keras_output = layer(keras_input)
-
     decomon_output = decomon_layer(decomon_inputs)
 
     # check output shapes
@@ -76,4 +66,5 @@ def test_decomon_activation(
         ibp=ibp,
         affine=affine,
         propagation=propagation,
+        decimal=decimal,
     )
