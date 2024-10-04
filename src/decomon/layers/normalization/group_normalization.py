b4c93de789fce7d404f508e7ade7d6c2192f057c
ducoffeM <melanie.ducoffe@airbus.com>
2024-10-04 09:22:11 +0200

    decomon implem for GroupNormalization

---
+++ src/decomon/layers/normalization/group_normalization.py
@@ -0,0 +1,14 @@
+from keras.layers import GroupNormalization
+from decomon.layers import DecomonLayer
+
+from decomon.types import Tensor
+from decomon.layers.utils import get_affine_representation_with_bias
+
+class DecomonGroupNormalization(DecomonLayer):
+    layer: GroupNormalization
+    linear=True
+    diagonal=True
+
+    def get_affine_representation(self) -> tuple[Tensor, Tensor]:
+
+        return get_affine_representation_with_bias(self.layer, diagonal = self.diagonal)
