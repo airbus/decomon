f1b7f82bd22b5249bb0854111b4390f9b2986ca3
ducoffeM <melanie.ducoffe@airbus.com>
2024-10-04 08:26:50 +0200

    decomon implem Cropping1D

--- src/decomon/layers/__init__.py
+++ src/decomon/layers/__init__.py
@@ -3,4 +3,4 @@ from .core.dense import DecomonDense
 from .layer import DecomonLayer
 from .merging.add import DecomonAdd
 from .convolutional import DecomonConv2D
-from .reshaping import DecomonZeroPadding1D, DecomonZeroPadding2D, DecomonZeroPadding3D
+from .reshaping import DecomonCropping1D, DecomonZeroPadding1D, DecomonZeroPadding2D, DecomonZeroPadding3D
---
+++ src/decomon/layers/reshaping/cropping1d.py
@@ -0,0 +1,14 @@
+from keras.layers import Cropping1D
+from decomon.layers import DecomonLayer
+
+from decomon.types import Tensor
+from decomon.layers.utils import get_affine_representation_wo_bias
+
+
+class DecomonCropping1D(DecomonLayer):
+    layer: Cropping1D
+    linear = True
+
+    def get_affine_representation(self) -> tuple[Tensor, Tensor]:
+
+        return get_affine_representation_wo_bias(self.layer, diagonal=self.diagonal)
