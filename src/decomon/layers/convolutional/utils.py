4d692d6f253afe78929ee78fed7737c00a7eb950
ducoffeM <melanie.ducoffe@airbus.com>
2024-10-11 10:51:32 +0200

    split get_toeplitz into two functions for reusing for maxpooling

--- src/decomon/layers/convolutional/utils.py
+++ src/decomon/layers/convolutional/utils.py
@@ -9,7 +9,15 @@ from keras.ops.image import extract_patches
 from decomon.types import Tensor
 
 
-def get_toeplitz(conv_layer: Conv2D) -> Tensor:
+def get_toeplitz_from_layer(conv_layer: Conv2D) -> Tensor:
+
+    kernel = conv_layer.kernel
+    input_shape = list(conv_layer.input.shape[1:])
+    output_shape = list(conv_layer.output.shape[1:])
+    config = conv_layer.get_config()
+    
+    return get_toeplitz(kernel, input_shape, output_shape, config)
+
     """Express formally the affine component of the convolution
     Conv is a linear operator but its affine component is implicit
     we use im2col and extract_patches to express the affine matrix
@@ -47,6 +55,57 @@ def get_toeplitz(conv_layer: Conv2D) -> Tensor:
     toeplitz = K.reshape(diag_patches, [N] + list(conv_layer.kernel.shape)[:-1] + [1] + output_shape_)
     kernel_ext = K.reshape(conv_layer.kernel, [1] + list(conv_layer.kernel.shape) + [1] * len(output_shape_))
     axis_to_sum = [i + 1 for i in range(len(conv_layer.kernel.shape) - 1)]
+
+    result = K.sum(toeplitz * kernel_ext, axis_to_sum)  # (N_in, n_filter, output_shape_)
+
+    if data_format == "channels_last":
+        axis_permute = (0, 2, 3, 1)
+        result = K.transpose(result, axis_permute)
+
+    result = K.reshape(result, input_shape + output_shape)
+
+    return result
+
+def get_toeplitz(kernel, input_shape, output_shape, config) -> Tensor:
+    """Express formally the affine component of the convolution
+    Conv is a linear operator but its affine component is implicit
+    we use im2col and extract_patches to express the affine matrix
+    Note that this matrix is Toeplitz
+
+    Args:
+        kernel:
+        input_shape:
+        output_shape:
+        config
+
+    Returns:
+         the affine operator W: conv(x)= Wx + bias
+    """
+
+    #input_shape = list(conv_layer.input.shape[1:])
+    #output_shape = list(conv_layer.output.shape[1:])
+    N = np.prod(input_shape)
+
+    diag = K.reshape(K.eye(N), [-1] + input_shape)
+
+    size = config["kernel_size"]
+    strides = config["strides"]
+    dilation_rate = config["dilation_rate"]
+    padding = config["padding"]
+    data_format = config["data_format"]
+
+    diag_patches = extract_patches(
+        images=diag, size=size, strides=strides, dilation_rate=dilation_rate, padding=padding, data_format=data_format
+    )
+
+    if data_format == "channels_first":
+        output_shape_ = output_shape[1:]
+    else:
+        output_shape_ = output_shape[:-1]
+
+    toeplitz = K.reshape(diag_patches, [N] + list(kernel.shape)[:-1] + [1] + output_shape_)
+    kernel_ext = K.reshape(kernel, [1] + list(kernel.shape) + [1] * len(output_shape_))
+    axis_to_sum = [i + 1 for i in range(len(kernel.shape) - 1)]
 
     result = K.sum(toeplitz * kernel_ext, axis_to_sum)  # (N_in, n_filter, output_shape_)
 
