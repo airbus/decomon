5ddb82d1eb7daa31566a4106a96775bdc71de8e7
ducoffeM <melanie.ducoffe@airbus.com>
2024-10-16 21:07:24 +0200

    decomon implem for dropout

---
+++ src/decomon/layers/regularization/dropout.py
@@ -0,0 +1,15 @@
+# onnx should use a custom library of keras...
+from keras.layers import Dropout
+from decomon.layers import DecomonLayer
+from decomon.types import Tensor
+from decomon.layers.utils.affine import get_affine_representation_wo_bias
+
+
+class DecomonDropout(DecomonLayer):
+    layer: Dropout
+    linear = True
+    diagonal = True
+
+    def get_affine_representation(self) -> tuple[Tensor, Tensor]:
+
+        return get_affine_representation_wo_bias(self.layer, diagonal=self.diagonal)
