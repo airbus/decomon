0a992c6a84aad474d80ff118bce2d59fe530e3fb
ducoffeM <melanie.ducoffe@airbus.com>
2025-01-09 13:45:34 +0100

    Add DecomonLinear and integrate with Reshape; implement affine propagation via layer_backward
    - Introduced the DecomonLinear class for streamlined handling of linear operations.
    - Updated the Reshape implementation to use DecomonLinear, improving modularity and consistency.
    - Enabled affine propagation through the layer_backward method, leveraging the jacobinet library for enhanced computational efficiency.

--- src/decomon/layers/reshaping/reshape.py
+++ src/decomon/layers/reshaping/reshape.py
@@ -1,12 +1,14 @@
 from keras.layers import Reshape
-from decomon.layers import DecomonLayer
+from decomon.layers import DecomonLayer, DecomonLinearLayer
 import keras.ops as K
 
 from decomon.types import Tensor
 from decomon.layers.utils import get_affine_representation_wo_bias
 
+class DecomonReshape(DecomonLinearLayer):
+    increasing= True
 
-class DecomonReshape(DecomonLayer):
+class DecomonReshape_old(DecomonLayer):
     layer: Reshape
     linear = True
     increasing = True
