f0a151ac2e1a1866ba7b20ffe240e557a892025f
ducoffeM <melanie.ducoffe@airbus.com>
2025-01-09 15:29:04 +0100

    Replace DecomonCropping2D inheritance with DecomonLinearLayer for consistency
    - Updated DecomonCropping2D to inherit from DecomonLinearLayer instead of its previous base.
    - Improves code structure and aligns with the updated design pattern for linear layers.

--- src/decomon/layers/reshaping/cropping2d.py
+++ src/decomon/layers/reshaping/cropping2d.py
@@ -1,15 +1,10 @@
 from keras.layers import Cropping2D
-from decomon.layers import DecomonLayer
+from decomon.layers import DecomonLayer, DecomonLinearLayer
 
 from decomon.types import Tensor
 from decomon.layers.utils import get_affine_representation_wo_bias
 
 
-class DecomonCropping2D(DecomonLayer):
+class DecomonCropping2D(DecomonLinearLayer):
     layer: Cropping2D
-    linear = True
     increasing = True
-
-    def get_affine_representation(self) -> tuple[Tensor, Tensor]:
-
-        return get_affine_representation_wo_bias(self.layer, diagonal=self.diagonal)
