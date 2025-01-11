cc06c4492d0b69b19e295c5fb872d8f3cb8cc536
ducoffeM <melanie.ducoffe@airbus.com>
2025-01-11 19:18:58 +0100

    Implement DepthwiseConv1D
    Add the import into init.py files on modules convolutional and layers
    Modify check_layer to change axis in 3D tensor parameters
    Create unit test’

--- src/decomon/layers/convolutional/__init__.py
+++ src/decomon/layers/convolutional/__init__.py
@@ -3,3 +3,5 @@ from .conv2d import DecomonConv2D
 from .conv1d import DecomonConv1D
 from .conv3d import DecomonConv3D
 from .depthwise_conv2d import DecomonDepthwiseConv2D
+from .depthwise_conv1d import DecomonDepthwiseConv1D
+
