26b4c3f80a6db13ba13f5dc16528e22a12596591
ducoffeM <melanie.ducoffe@airbus.com>
2025-01-19 19:24:19 +0100

    temporary fix to get into a new branch

--- src/decomon/layers/core/dense.py
+++ src/decomon/layers/core/dense.py
@@ -9,7 +9,7 @@ from decomon.types import Tensor
 from .utils import Dense_kernel_constraint
 
 
-class DecomonDense(DecomonLayer):
+class DecomonDense(DecomonLinearLayer):
 
     def __init__(
         self,
