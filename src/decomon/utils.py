93d651256ec4c5aef39f1b69d46b4d62b09514da
ducoffeM <melanie.ducoffe@airbus.com>
2025-04-23 16:42:42 +0200

    Add import decomon.utils

---
+++ src/decomon/utils.py
@@ -0,0 +1,11 @@
+# set global variables
+import numpy as np
+
+# memory_limit = int(2e9)
+memory_limit = 5000000
+# if the affine weights of size N, M is such that (N*M)>= memory_limit, we opt for implicit affine propagation or block splitting
+
+
+def fit_memory(input_shape, output_shape):
+    inner_dim: int = int(np.prod(input_shape) * np.prod(output_shape))
+    return inner_dim <= memory_limit
