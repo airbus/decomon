dfe8bc39916c9d3be2e9e3a3fee240b56860fcf1
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2025-05-05 14:22:03 +0200

    Restore tests

---
+++ tests/test_decomon_layer.py
@@ -0,0 +1,330 @@
+import keras.ops as K
+import pytest
+from keras.layers import Dense, Input
+
+from decomon.constants import Propagation
+from decomon.layers.layer import DecomonLayer
+from decomon.perturbation_domain import BoxDomain
+
+
+def test_decomon_layer_nok_unbuilt_keras_layer():
+    layer = Dense(3)
+    with pytest.raises(ValueError):
+        DecomonLayer(layer=layer)
+
+
+def test_decomon_layer_nok_ibp_affine():
+    layer = Dense(3)
+    layer(Input((1,)))
+    with pytest.raises(ValueError):
+        DecomonLayer(layer=layer, ibp=False, affine=False)
+
+
+def test_decomon_layer_nok_backward_no_model_output_shape():
+    layer = Dense(3)
+    layer(Input((1,)))
+    with pytest.raises(ValueError):
+        DecomonLayer(layer=layer, propagation=Propagation.BACKWARD)
+
+
+def test_decomon_layer_extra_kwargs():
+    layer = Dense(3)
+    layer(Input((1,)))
+    DecomonLayer(layer=layer, alpha="jet")
+
+
+class MyLinearDecomonDense1d(DecomonLayer):
+    linear = True
+    layer: Dense
+
+    def get_affine_representation(self):
+        return self.layer.kernel, self.layer.bias
+
+
+class MyNonLinearDecomonDense1d(DecomonLayer):
+    linear = False
+    layer: Dense
+
+    def get_affine_bounds(self, lower, upper):
+        batchsize = lower.shape[0]
+        extended_kernel = K.repeat(self.layer.kernel[None], batchsize, axis=0)
+        extended_bias = K.repeat(self.layer.bias[None], batchsize, axis=0)
+        return extended_kernel, extended_bias, extended_kernel, extended_bias
+
+    def forward_ibp_propagate(self, lower, upper):
+        z_value = K.cast(0.0, dtype=lower.dtype)
+        kernel_pos = K.maximum(z_value, self.layer.kernel)
+        kernel_neg = K.minimum(z_value, self.layer.kernel)
+        u_c = K.dot(upper, kernel_pos) + K.dot(lower, kernel_neg)
+        l_c = K.dot(lower, kernel_pos) + K.dot(upper, kernel_neg)
+        return l_c, u_c
+
+
+@pytest.mark.parametrize("singlelayer_model", [False, True])
+def test_my_decomon_dense_1d(singlelayer_model, ibp, affine, propagation, helpers):
+    # input/output shapes
+    batchsize = 10
+    layer_input_dim = 2
+    layer_output_dim = 7
+    perturbation_domain = BoxDomain()
+    model_output_shape_if_no_singlelayer_model = (3, 5)
+    model_input_dim_if_no_singlelayer_model = 9
+
+    layer_input_shape = (layer_input_dim,)
+    layer_output_shape = (layer_output_dim,)
+
+    if singlelayer_model:
+        model_input_dim = layer_input_dim
+        model_output_shape = layer_output_shape
+    else:
+        model_input_dim = model_input_dim_if_no_singlelayer_model
+        model_output_shape = model_output_shape_if_no_singlelayer_model
+
+    model_input_shape = (model_input_dim,)
+
+    # keras layer
+    layer = Dense(units=layer_output_dim)
+    layer(Input((layer_input_dim,)))
+
+    # decomon layers
+    linear_decomon_layer = MyLinearDecomonDense1d(
+        layer=layer,
+        ibp=ibp,
+        affine=affine,
+        propagation=propagation,
+        perturbation_domain=perturbation_domain,
+        model_output_shape=model_output_shape,
+        model_input_shape=model_input_shape,
+    )
+    nonlinear_decomon_layer = MyNonLinearDecomonDense1d(
+        layer=layer,
+        ibp=ibp,
+        affine=affine,
+        propagation=propagation,
+        perturbation_domain=perturbation_domain,
+        model_output_shape=model_output_shape,
+        model_input_shape=model_input_shape,
+    )
+
+    # symbolic inputs
+    decomon_inputs_linear = helpers.get_decomon_symbolic_inputs(
+        model_input_shape=model_input_shape,
+        model_output_shape=model_output_shape,
+        layer_input_shape=layer_input_shape,
+        layer_output_shape=layer_output_shape,
+        ibp=ibp,
+        affine=affine,
+        propagation=propagation,
+        perturbation_domain=perturbation_domain,
+        for_linear_layer=True,
+    )
+    decomon_inputs_nonlinear = helpers.get_decomon_symbolic_inputs(
+        model_input_shape=model_input_shape,
+        model_output_shape=model_output_shape,
+        layer_input_shape=layer_input_shape,
+        layer_output_shape=layer_output_shape,
+        ibp=ibp,
+        affine=affine,
+        propagation=propagation,
+        perturbation_domain=perturbation_domain,
+        for_linear_layer=False,
+    )
+
+    # actual (random) tensors + expected output shapes
+    (
+        affine_bounds_to_propagate,
+        constant_oracle_bounds,
+        perturbation_domain_inputs,
+    ) = nonlinear_decomon_layer.inputs_outputs_spec.split_inputs(decomon_inputs_nonlinear)
+    perturbation_domain_inputs_val = [
+        helpers.generate_random_tensor(x.shape[1:], batchsize=batchsize) for x in perturbation_domain_inputs
+    ]
+
+    if affine:
+        if propagation == Propagation.FORWARD:
+            b_out_shape = layer_output_shape
+            w_out_shape = model_input_shape + layer_output_shape
+        else:
+            b_out_shape = model_output_shape
+            w_out_shape = layer_input_shape + model_output_shape
+        affine_bounds_to_propagate_val = [
+            helpers.generate_random_tensor(tensor.shape[1:], batchsize=batchsize)
+            for tensor in affine_bounds_to_propagate
+        ]
+        propagated_affine_bounds_expected_shape = [w_out_shape, b_out_shape, w_out_shape, b_out_shape]
+    else:
+        propagated_affine_bounds_expected_shape = []
+        affine_bounds_to_propagate_val = []
+
+    if ibp:
+        constant_oracle_bounds_val = [
+            helpers.generate_random_tensor(tensor.shape[1:], batchsize=batchsize) for tensor in constant_oracle_bounds
+        ]
+        propagated_ibp_bounds_expected_shape = [layer_output_shape, layer_output_shape]
+    else:
+        propagated_ibp_bounds_expected_shape = []
+        constant_oracle_bounds_val = []
+
+    # linear case only: no constant bounds in backward
+    if linear_decomon_layer.inputs_outputs_spec.needs_constant_bounds_inputs():
+        constant_oracle_bounds_val_linear = constant_oracle_bounds_val
+    else:
+        constant_oracle_bounds_val_linear = []
+
+    decomon_inputs_val_linear = linear_decomon_layer.inputs_outputs_spec.flatten_inputs(
+        affine_bounds_to_propagate=affine_bounds_to_propagate_val,
+        constant_oracle_bounds=constant_oracle_bounds_val_linear,
+        perturbation_domain_inputs=perturbation_domain_inputs_val,
+    )
+    decomon_inputs_val_nonlinear = nonlinear_decomon_layer.inputs_outputs_spec.flatten_inputs(
+        affine_bounds_to_propagate=affine_bounds_to_propagate_val,
+        constant_oracle_bounds=constant_oracle_bounds_val,
+        perturbation_domain_inputs=perturbation_domain_inputs_val,
+    )
+
+    if propagation == Propagation.FORWARD:
+        decomon_output_expected_shapes = propagated_affine_bounds_expected_shape + propagated_ibp_bounds_expected_shape
+    else:
+        decomon_output_expected_shapes = propagated_affine_bounds_expected_shape
+
+    # symbolic call
+    linear_decomon_output = linear_decomon_layer(decomon_inputs_linear)
+    nonlinear_decomon_output = nonlinear_decomon_layer(decomon_inputs_nonlinear)
+
+    # shapes ok ?
+    linear_decomon_output_shape_from_call = [tensor.shape[1:] for tensor in linear_decomon_output]
+    assert linear_decomon_output_shape_from_call == decomon_output_expected_shapes
+    nonlinear_decomon_output_shape_from_call = [tensor.shape[1:] for tensor in nonlinear_decomon_output]
+    assert nonlinear_decomon_output_shape_from_call == decomon_output_expected_shapes
+
+    # actual call
+    linear_decomon_output_val = linear_decomon_layer(decomon_inputs_val_linear)
+    nonlinear_decomon_output_val = nonlinear_decomon_layer(decomon_inputs_val_nonlinear)
+
+    # shapes ok ?
+    linear_decomon_output_shape_from_call = [tensor.shape[1:] for tensor in linear_decomon_output_val]
+    assert linear_decomon_output_shape_from_call == decomon_output_expected_shapes
+    nonlinear_decomon_output_shape_from_call = [tensor.shape[1:] for tensor in linear_decomon_output_val]
+    assert nonlinear_decomon_output_shape_from_call == decomon_output_expected_shapes
+
+    # same values ?
+    helpers.assert_decomon_outputs_equal(linear_decomon_output_val, nonlinear_decomon_output_val)
+
+    # inequalities hold ?
+    # We only check it in case of a single-layer model => model shapes == layer shapes
+    # And with identity affine bounds + constant bounds = x = [keras_input, keras_input]
+    if model_input_shape == layer_input_shape and model_output_shape == layer_output_shape:
+        # keras input
+        keras_input_val = helpers.generate_random_tensor(layer_input_shape, batchsize=batchsize)
+
+        # new decomon inputs values
+        decomon_inputs_val = helpers.generate_simple_decomon_layer_inputs_from_keras_input(
+            keras_input=keras_input_val,
+            layer_output_shape=layer_output_shape,
+            ibp=ibp,
+            affine=affine,
+            propagation=propagation,
+            perturbation_domain=perturbation_domain,
+            for_linear_layer=False,
+        )
+
+        # linear case only: no constant bounds in backward
+        if linear_decomon_layer.inputs_outputs_spec.needs_constant_bounds_inputs():
+            decomon_inputs_val_linear = decomon_inputs_val
+        else:
+            (
+                affine_bounds_to_propagate_val,
+                constant_oracle_bounds_val,
+                perturbation_domain_inputs_val,
+            ) = nonlinear_decomon_layer.inputs_outputs_spec.split_inputs(decomon_inputs_val)
+            decomon_inputs_val_linear = linear_decomon_layer.inputs_outputs_spec.flatten_inputs(
+                affine_bounds_to_propagate=affine_bounds_to_propagate_val,
+                constant_oracle_bounds=[],
+                perturbation_domain_inputs=perturbation_domain_inputs_val,
+            )
+
+        # decomon call
+        linear_decomon_output_val = linear_decomon_layer(decomon_inputs_val_linear)
+        nonlinear_decomon_output_val = nonlinear_decomon_layer(decomon_inputs_val)
+
+        # keras call
+        keras_output_val = layer(keras_input_val)
+
+        # comparison
+        helpers.assert_decomon_output_compare_with_keras_input_output_single_layer(
+            decomon_output=linear_decomon_output_val,
+            keras_output=keras_output_val,
+            keras_input=keras_input_val,
+            ibp=ibp,
+            affine=affine,
+            propagation=propagation,
+        )
+        helpers.assert_decomon_outputs_equal(linear_decomon_output_val, nonlinear_decomon_output_val)
+
+
+@pytest.mark.parametrize("affine", [True])
+def test_check_affine_bounds_characteristics(
+    ibp,
+    affine,
+    propagation,
+    perturbation_domain,
+    empty,
+    diag,
+    nobatch,
+    simple_keras_symbolic_model_input_fn,
+    simple_keras_symbolic_layer_input_fn,
+    simple_decomon_symbolic_input_fn,
+    simple_keras_model_input_fn,
+    simple_keras_layer_input_fn,
+    simple_decomon_input_fn,
+    helpers,
+):
+    units = 7
+
+    # init + build keras layer
+    keras_symbolic_model_input = simple_keras_symbolic_model_input_fn()
+    keras_symbolic_layer_input = simple_keras_symbolic_layer_input_fn(keras_symbolic_model_input)
+    layer = Dense(units=units)
+    layer(keras_symbolic_layer_input)
+
+    # init + build decomon layer
+    output_shape = layer.output.shape[1:]
+    model_output_shape = output_shape
+    decomon_symbolic_inputs = simple_decomon_symbolic_input_fn(output_shape=output_shape, linear=False)
+    decomon_layer = DecomonLayer(
+        layer=layer,
+        ibp=ibp,
+        affine=affine,
+        propagation=propagation,
+        perturbation_domain=perturbation_domain,
+        model_output_shape=model_output_shape,
+    )
+
+    # actual inputs
+    keras_model_input = simple_keras_model_input_fn()
+    keras_layer_input = simple_keras_layer_input_fn(keras_model_input)
+    decomon_inputs = simple_decomon_input_fn(
+        keras_model_input=keras_model_input,
+        keras_layer_input=keras_layer_input,
+        output_shape=output_shape,
+        linear=False,
+    )
+
+    if affine:
+        affine_bounds, _, _ = decomon_layer.inputs_outputs_spec.split_inputs(inputs=decomon_symbolic_inputs)
+        affine_bounds_shape = [t.shape for t in affine_bounds]
+        assert decomon_layer.inputs_outputs_spec.is_identity_bounds(affine_bounds) is empty
+        assert decomon_layer.inputs_outputs_spec.is_diagonal_bounds(affine_bounds) is diag
+        assert decomon_layer.inputs_outputs_spec.is_wo_batch_bounds(affine_bounds) is nobatch
+        assert decomon_layer.inputs_outputs_spec.is_identity_bounds_shape(affine_bounds_shape) is empty
+        assert decomon_layer.inputs_outputs_spec.is_diagonal_bounds_shape(affine_bounds_shape) is diag
+        assert decomon_layer.inputs_outputs_spec.is_wo_batch_bounds_shape(affine_bounds_shape) is nobatch
+
+        affine_bounds, _, _ = decomon_layer.inputs_outputs_spec.split_inputs(inputs=decomon_inputs)
+        affine_bounds_shape = [t.shape for t in affine_bounds]
+        assert decomon_layer.inputs_outputs_spec.is_identity_bounds(affine_bounds) is empty
+        assert decomon_layer.inputs_outputs_spec.is_diagonal_bounds(affine_bounds) is diag
+        assert decomon_layer.inputs_outputs_spec.is_wo_batch_bounds(affine_bounds) is nobatch
+        assert decomon_layer.inputs_outputs_spec.is_identity_bounds_shape(affine_bounds_shape) is empty
+        assert decomon_layer.inputs_outputs_spec.is_diagonal_bounds_shape(affine_bounds_shape) is diag
+        assert decomon_layer.inputs_outputs_spec.is_wo_batch_bounds_shape(affine_bounds_shape) is nobatch
---
+++ tests/test_output.py
@@ -0,0 +1,254 @@
+import keras.ops as K
+import numpy as np
+import pytest
+from pytest_cases import parametrize
+
+from decomon.constants import Propagation
+from decomon.layers.inputs_outputs_specs import InputsOutputsSpec
+from decomon.layers.output import ConvertOutput
+from decomon.perturbation_domain import BoxDomain
+
+
+def generate_simple_inputs(
+    ibp,
+    affine,
+    input_shape,
+    output_shape,
+    batchsize,
+    diag,
+    nobatch,
+    perturbation_domain,
+    needs_perturbation_domain_inputs=False,
+):
+    if ibp:
+        x = K.convert_to_tensor(2.0 * np.random.random((batchsize,) + output_shape) - 1.0)
+        lower = x - 0.5
+        upper = x + 0.5
+        constant_bounds = [lower, upper]
+    else:
+        constant_bounds = []
+    if affine:
+        if diag:
+            w = K.ones(input_shape)
+        else:
+            w = K.reshape(K.eye(int(np.prod(input_shape)), int(np.prod(output_shape))), input_shape + output_shape)
+        b = 0.1 * K.ones(output_shape)
+        if not nobatch:
+            w = K.repeat(w[None], batchsize, axis=0)
+            b = K.repeat(b[None], batchsize, axis=0)
+        affine_bounds = [w, -b, w, b]
+    else:
+        affine_bounds = []
+
+    if needs_perturbation_domain_inputs:
+        if isinstance(perturbation_domain, BoxDomain):
+            keras_input = K.convert_to_tensor(2.0 * np.random.random((batchsize,) + input_shape) - 1.0)
+            perturbation_domain_inputs = [
+                K.concatenate([keras_input[:, None] - 0.1, keras_input[:, None] + 0.1], axis=1)
+            ]
+        else:
+            raise NotImplementedError
+    else:
+        perturbation_domain_inputs = []
+
+    return affine_bounds + constant_bounds + perturbation_domain_inputs
+
+
+@parametrize("ibp_from, affine_from", [(True, False), (False, True), (True, True)], ids=["ibp", "affine", "hybrid"])
+@parametrize("ibp_to, affine_to", [(True, False), (False, True), (True, True)], ids=["ibp", "affine", "hybrid"])
+@parametrize(
+    "diag, nobatch",
+    [
+        (True, True),
+        (True, False),
+        (False, True),
+        (False, False),
+    ],
+    ids=["diagonal-nobatch", "diagonal", "nobatch", "generic"],
+)
+@parametrize("input_shape", [(3,), (5, 6, 2)], ids=["1d", "multid"])
+@parametrize("output_shape", [(2,), (4, 5, 1)], ids=["1d", "multid"])
+def test_convert_1output(
+    ibp_from,
+    affine_from,
+    ibp_to,
+    affine_to,
+    diag,
+    nobatch,
+    input_shape,
+    output_shape,
+    batchsize,
+    perturbation_domain,
+    helpers,
+):
+    if not affine_from:
+        if diag or nobatch:
+            pytest.skip("diag and nobatch have no meaning when affine is False")
+    if diag:
+        if len(input_shape) != len(output_shape):
+            pytest.skip("we need input_shape==output_shape if diag")
+        else:
+            output_shape = input_shape
+
+    layer = ConvertOutput(
+        perturbation_domain=perturbation_domain,
+        ibp_from=ibp_from,
+        affine_from=affine_from,
+        ibp_to=ibp_to,
+        affine_to=affine_to,
+        model_output_shapes=[output_shape],
+    )
+
+    symbolic_input = helpers.get_decomon_symbolic_inputs(
+        model_input_shape=input_shape,
+        model_output_shape=None,
+        layer_input_shape=output_shape,
+        layer_output_shape=None,
+        ibp=ibp_from,
+        affine=affine_from,
+        propagation=Propagation.FORWARD,
+        perturbation_domain=perturbation_domain,
+        diag=diag,
+        nobatch=nobatch,
+        remove_perturbation_domain_inputs=True,
+        add_perturbation_domain_inputs=layer.needs_perturbation_domain_inputs(),
+    )
+
+    input = generate_simple_inputs(
+        ibp=ibp_from,
+        affine=affine_from,
+        input_shape=input_shape,
+        output_shape=output_shape,
+        batchsize=batchsize,
+        diag=diag,
+        nobatch=nobatch,
+        perturbation_domain=perturbation_domain,
+        needs_perturbation_domain_inputs=layer.needs_perturbation_domain_inputs(),
+    )
+
+    symbolic_output = layer(symbolic_input)
+    output = layer(input)
+
+    # check shapes
+    output_shape = [t.shape for t in output]
+    expected_output_shape = [t.shape for t in symbolic_output]
+    expected_output_shape = helpers.replace_none_by_batchsize(shapes=expected_output_shape, batchsize=batchsize)
+    assert output_shape == expected_output_shape
+
+    inputs_outputs_spec_fused = InputsOutputsSpec(
+        ibp=ibp_to, affine=layer.affine_to, layer_input_shape=layer.model_output_shapes, is_merging_layer=True
+    )
+    assert len(output_shape) == inputs_outputs_spec_fused.nb_output_tensors * layer.nb_outputs_keras_model
+
+
+@parametrize("ibp_from, affine_from", [(True, False), (False, True), (True, True)], ids=["ibp", "affine", "hybrid"])
+@parametrize("ibp_to, affine_to", [(True, False), (False, True), (True, True)], ids=["ibp", "affine", "hybrid"])
+@parametrize(
+    "diag_1, nobatch_1",
+    [
+        (True, True),
+        (True, False),
+        (False, True),
+        (False, False),
+    ],
+    ids=["diagonal-nobatch", "diagonal", "nobatch", "generic"],
+)
+@parametrize("input_shape", [(3,), (5, 6, 2)], ids=["1d", "multid"])
+@parametrize("output_shape_1", [(2,), (4, 5, 1)], ids=["1d", "multid"])
+@parametrize("output_shape_2", [(3,), (2, 5, 1)], ids=["1d", "multid"])
+def test_convert_2outputs(
+    ibp_from,
+    affine_from,
+    ibp_to,
+    affine_to,
+    diag_1,
+    nobatch_1,
+    input_shape,
+    output_shape_1,
+    output_shape_2,
+    batchsize,
+    perturbation_domain,
+    helpers,
+):
+    if not affine_from:
+        if diag_1 or nobatch_1:
+            pytest.skip("diag and nobatch have no meaning when affine is False")
+    if diag_1:
+        if len(input_shape) != len(output_shape_1):
+            pytest.skip("we need input_shape==output_shape if diag")
+        else:
+            output_shape_1 = input_shape
+
+    model_output_shapes = [output_shape_1, output_shape_2]
+    layer = ConvertOutput(
+        perturbation_domain=perturbation_domain,
+        ibp_from=ibp_from,
+        affine_from=affine_from,
+        ibp_to=ibp_to,
+        affine_to=affine_to,
+        model_output_shapes=model_output_shapes,
+    )
+
+    symbolic_input_1 = helpers.get_decomon_symbolic_inputs(
+        model_input_shape=input_shape,
+        model_output_shape=None,
+        layer_input_shape=output_shape_1,
+        layer_output_shape=None,
+        ibp=ibp_from,
+        affine=affine_from,
+        propagation=Propagation.FORWARD,
+        perturbation_domain=perturbation_domain,
+        diag=diag_1,
+        nobatch=nobatch_1,
+        remove_perturbation_domain_inputs=True,
+    )
+    symbolic_input_2 = helpers.get_decomon_symbolic_inputs(
+        model_input_shape=input_shape,
+        model_output_shape=None,
+        layer_input_shape=output_shape_2,
+        layer_output_shape=None,
+        ibp=ibp_from,
+        affine=affine_from,
+        propagation=Propagation.FORWARD,
+        perturbation_domain=perturbation_domain,
+        remove_perturbation_domain_inputs=True,
+        add_perturbation_domain_inputs=layer.needs_perturbation_domain_inputs(),
+    )
+    symbolic_input = symbolic_input_1 + symbolic_input_2
+
+    input_1 = generate_simple_inputs(
+        ibp=ibp_from,
+        affine=affine_from,
+        input_shape=input_shape,
+        output_shape=output_shape_1,
+        batchsize=batchsize,
+        diag=diag_1,
+        nobatch=nobatch_1,
+        perturbation_domain=perturbation_domain,
+    )
+    input_2 = generate_simple_inputs(
+        ibp=ibp_from,
+        affine=affine_from,
+        input_shape=input_shape,
+        output_shape=output_shape_2,
+        batchsize=batchsize,
+        perturbation_domain=perturbation_domain,
+        needs_perturbation_domain_inputs=layer.needs_perturbation_domain_inputs(),
+        diag=False,
+        nobatch=False,
+    )
+    input = input_1 + input_2
+
+    symbolic_output = layer(symbolic_input)
+    output = layer(input)
+
+    # check shapes
+    output_shape = [t.shape for t in output]
+    expected_output_shape = [t.shape for t in symbolic_output]
+    expected_output_shape = helpers.replace_none_by_batchsize(shapes=expected_output_shape, batchsize=batchsize)
+    assert output_shape == expected_output_shape
+
+    inputs_outputs_spec_fused = InputsOutputsSpec(
+        ibp=ibp_to, affine=layer.affine_to, layer_input_shape=layer.model_output_shapes, is_merging_layer=True
+    )
+    assert len(output_shape) == inputs_outputs_spec_fused.nb_output_tensors * layer.nb_outputs_keras_model
