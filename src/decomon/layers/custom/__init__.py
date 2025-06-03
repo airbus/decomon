368312416f0630c5848710bc997d4ea481d7d256
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2025-06-03 15:48:03 +0200

    Refactor custom decomon layers + fix decomon_max forward affine

--- src/decomon/layers/custom/__init__.py
+++ src/decomon/layers/custom/__init__.py
@@ -1,4 +1,2 @@
-from .max import DecomonMax
-from .min import DecomonMin
-from .mul import DecomonMulConstant
-from .utils import get_affine_lower_bound_max, get_affine_upper_bound_max
+from .constant import DecomonMulConstant
+from .reduce import DecomonMax, DecomonMin
