9f59ee9762301005f93e3e40ad04cf23ca03b5de
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2025-09-01 15:18:06 +0200

    Do not implement decomon layers for non-linear normalization
    Unit, Layer and Group normalization are not linear.
    For now we remove linear attribute from the decomon counterparts
    and do not implement yet their ibp/affine bounds, nor test them.

--- src/decomon/layers/normalization/unit_normalization.py
+++ src/decomon/layers/normalization/unit_normalization.py
@@ -1,14 +1,8 @@
 from keras.layers import UnitNormalization
 
 from decomon.layers import DecomonLayer
-from decomon.layers.utils import get_affine_representation_with_bias
-from decomon.types import Tensor
 
 
 class DecomonUnitNormalization(DecomonLayer):
     layer: UnitNormalization
-    linear = True
     diagonal = True
-
-    def get_affine_representation(self) -> tuple[Tensor, Tensor]:
-        return get_affine_representation_with_bias(self.layer, diagonal=self.diagonal)
